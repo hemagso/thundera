59b4a426f22bb3b683ad97d8b858b69f9e411477
Henrique Magalhães Soares <hemagso@gmail.com>
2024-04-13 22:28:33 +0000

    Add range semantics

--- tests/test_domains.py
+++ tests/test_domains.py
@@ -26,20 +26,16 @@ def spark() -> Generator[SparkSession, None, None]:
 
 
 @pytest.mark.parametrize("range, values, expected", [
-    ((-5, 5, True, True), [-6, -5.0001, -5, -4.9999, 0, 4.9999, 5, 5.0001, 6, None], "0011111000"), 
-    ((-5, 5, False, True), [-6, -5.0001, -5, -4.9999, 0, 4.9999, 5, 5.0001, 6, None], "0001111000"), 
-    ((-5, 5, True, False), [-6, -5.0001, -5, -4.9999, 0, 4.9999, 5, 5.0001, 6, None], "0011110000"), 
-    ((-5, 5, False, False), [-6, -5.0001, -5, -4.9999, 0, 4.9999, 5, 5.0001, 6, None], "0001110000"),
-    ((float("-inf"), 5, True, True), [-100_000_000, -100_000, -100, -4.9999, 0, 4.9999, 5, 5.0001, 6, None], "1111111000"), 
-    ((float("-inf"), 5, False, True), [-100_000_000, -100_000, -100, -4.9999, 0, 4.9999, 5, 5.0001, 6, None], "1111111000"), 
-    ((float("-inf"), 5, True, False), [-100_000_000, -100_000, -100, -4.9999, 0, 4.9999, 5, 5.0001, 6, None], "1111110000"), 
-    ((float("-inf"), 5, False, False), [-100_000_000, -100_000, -100, -4.9999, 0, 4.9999, 5, 5.0001, 6, None], "1111110000"),
-    ((-5, float("+inf"), True, True), [-6, -5.0001, -5, -4.9999, 0, 4.9999, 100, 100_000, 100_000_000, None], "0011111110"), 
-    ((-5, float("+inf"), False, True), [-6, -5.0001, -5, -4.9999, 0, 4.9999, 100, 100_000, 100_000_000, None], "0001111110"), 
-    ((-5, float("+inf"), True, False), [-6, -5.0001, -5, -4.9999, 0, 4.9999, 100, 100_000, 100_000_000, None], "0011111110"), 
-    ((-5, float("+inf"), False, False), [-6, -5.0001, -5, -4.9999, 0, 4.9999, 100, 100_000, 100_000_000, None], "0001111110") 
+    ("[-5, 5]", [-6, -5.0001, -5, -4.9999, 0, 4.9999, 5, 5.0001, 6, None], "0011111000"), 
+    ("(-5, 5]", [-6, -5.0001, -5, -4.9999, 0, 4.9999, 5, 5.0001, 6, None], "0001111000"), 
+    ("[-5, 5)", [-6, -5.0001, -5, -4.9999, 0, 4.9999, 5, 5.0001, 6, None], "0011110000"), 
+    ("(-5, 5)", [-6, -5.0001, -5, -4.9999, 0, 4.9999, 5, 5.0001, 6, None], "0001110000"),
+    ("(-inf, 5]", [-100_000_000, -100_000, -100, -4.9999, 0, 4.9999, 5, 5.0001, 6, None], "1111111000"), 
+    ("(-inf, 5)", [-100_000_000, -100_000, -100, -4.9999, 0, 4.9999, 5, 5.0001, 6, None], "1111110000"),
+    ("[-5, +inf)", [-6, -5.0001, -5, -4.9999, 0, 4.9999, 100, 100_000, 100_000_000, None], "0011111110"), 
+    ("(-5, +inf)", [-6, -5.0001, -5, -4.9999, 0, 4.9999, 100, 100_000, 100_000_000, None], "0001111110") 
 ])
-def test_range_domain_contains(spark: SparkSession, range: tuple, values: list[float], expected: list[bool]):
+def test_range_domain_contains(spark: SparkSession, range: str, values: list[float], expected: list[bool]):
     expected = bool_from_str(expected)
     data = spark.createDataFrame(
         data = [(None if v is None else float(v),) for v in values],
@@ -50,7 +46,7 @@ def test_range_domain_contains(spark: SparkSession, range: tuple, values: list[f
 
     domain = RangeDomain(
         description = "Test Range",
-        **dict(zip(("start", "end", "include_start", "include_end"), range))
+        value = range
     )
 
     results = data.withColumn("results", domain.contains(col("value")))
@@ -65,4 +61,4 @@ def test_range_domain_contains(spark: SparkSession, range: tuple, values: list[f
     ("(-5, 5)", (-5, 5, False, False))
 ])
 def test_parse_range(range: str, expected: tuple[float, float, bool, bool]):
-    assert parse_range(range) == expected
+    assert parse_range(range) == dict(zip(("start", "end", "include_start", "include_end"), expected))
--- thundera/metadata.py
+++ thundera/metadata.py
@@ -1,30 +1,55 @@
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, model_validator, field_validator
 from typing import Literal, Annotated
 from pyspark.sql import Column
 from pyspark.sql.functions import lit
+from typing import Any, Self
 
-
-def parse_range(range: str) -> tuple[float, float, bool, bool]:
+def parse_range(range: str) -> dict:
     range = range.strip()
 
-    include_start = range[0] == "["
-    include_end = range[-1] == "]"
-
+    open_range = range[0]
+    close_range = range[-1]
     range = range[1:-1]
+
+    if open_range not in ("(", "["):
+        raise ValueError("Invalid range format")
+    if close_range not in (")", "]"):
+        raise ValueError("Invalid range format")
+
+    include_start = open_range == "["
+    include_end = close_range == "]"
+
     start, end = (item.strip() for item in range.split(","))
     start = float("-inf") if start == "-inf" else float(start)
     end = float("inf") if end in ("inf", "+inf") else float(end)
 
-    return (start, end, include_start, include_end)
+    return {
+        "start": start, 
+        "end": end, 
+        "include_start": include_start, 
+        "include_end": include_end
+    }
 
 
-class RangeDomain(BaseModel):
-    type: Literal["range"] = "range"
+class Range(BaseModel):
     start: float
-    include_start: bool
     end: float
+    include_start: bool
     include_end: bool
-    description: str | None
+
+    @model_validator(mode="before")
+    @classmethod
+    def parse_interval(cls, range: dict | str) -> dict:
+        if isinstance(range, str):
+            range = parse_range(range)
+        return range
+        
+
+    @model_validator(mode="after")
+    def check_range_bounds(self) -> Self:
+        if self.start > self.end:
+            raise ValueError("Start value should be less of equal to end")
+        return self
 
     def contains(self, values: Column) -> Column:
         start = lit(self.start)
@@ -36,6 +61,14 @@ class RangeDomain(BaseModel):
         if self.include_end:
             return values.isNotNull() & (values > start) & (values <= end)
         return values.isNotNull() & (values > start) & (values < end)
+
+class RangeDomain(BaseModel):
+    type: Literal["range"] = "range"
+    value: Range
+    description: str | None
+
+    def contains(self, values: Column) -> Column:
+        return self.value.contains(values)
     
 
 class SingleDomain(BaseModel):
