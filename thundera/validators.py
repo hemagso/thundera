eb82ca56fed436c6eef27775eda8bc61949c2447
Henrique Magalhães Soares <hemagso@gmail.com>
2024-04-19 01:34:11 +0000

    Add domain_selector

--- thundera/validators.py
+++ thundera/validators.py
@@ -2,14 +2,25 @@ from functools import reduce
 from typing import Callable
 
 from pyspark.sql import Column
-from pyspark.sql.functions import lit
+from pyspark.sql.functions import lit, when
 
 from .metadata import AttributeField, Domain, RangeDomain, SingleDomain
 
 DomainValidator = Callable[[Column], Column]
+DomainTransformer = Callable[[Column], Column]
 
 
 def domain_contains(domain: Domain) -> DomainValidator:
+    """Returns a function that builds a spark expression that validates whether values
+    in a spark Column are within a range domain.
+
+    Args:
+        domain (Domain): The domain being checked against.
+
+    Returns:
+        DomainValidator: A function that returns the spark expression that performs
+            the validation.
+    """
     if domain.type == "range":
         return range_domain_contains(domain)
     if domain.type == "single":
@@ -19,6 +30,17 @@ def domain_contains(domain: Domain) -> DomainValidator:
 
 
 def range_domain_contains(domain: RangeDomain) -> DomainValidator:
+    """Returns a function that builds a spark expression that validates whether values
+    in a Spark Column are within a range domain.
+
+    Args:
+        domain (RangeDomain): The range domain being checked against.
+
+    Returns:
+        DomainValidator: A function that returns the spark expression that performs
+            the validation.
+    """
+
     def validator(values: Column) -> Column:
         range = domain.value
         start = lit(range.start)
@@ -36,6 +58,17 @@ def range_domain_contains(domain: RangeDomain) -> DomainValidator:
 
 
 def single_domain_contains(domain: SingleDomain) -> DomainValidator:
+    """Returns a function that builds a spark expression that validates whether values
+    in a Spark Column are within a single domain.
+
+    Args:
+        domain (SingleDomain): The single domain being checked against.
+
+    Returns:
+        DomainValidator: A function that returns the spark expression that performs
+            the validation.
+    """
+
     def validator(values: Column) -> Column:
         return values.isNotNull() & (values == lit(domain.value))
 
@@ -43,6 +76,14 @@ def single_domain_contains(domain: SingleDomain) -> DomainValidator:
 
 
 def null_domain_contains() -> DomainValidator:
+    """Returns a function that builds a spark expression that validates whether values
+    in a Spark Column are within a null domain.
+
+    Returns:
+        DomainValidator: A function that returns the spark expression that performs
+            the validation.
+    """
+
     def validator(values: Column) -> Column:
         return values.isNull()
 
@@ -50,6 +91,20 @@ def null_domain_contains() -> DomainValidator:
 
 
 def domain_validator(field: AttributeField) -> DomainValidator:
+    """Returns a function that builds a spark expression that validates whether values
+    in a Spark Column are within an Attribute Field domain.
+
+    Args:
+        field (AttributeField): The Attribute Field being checked against.
+
+    Raises:
+        ValueError: Raised if the Attribute Field being checked against does not have
+            any valid domains.
+
+    Returns:
+        DomainValidator: A function that returns the spark expression that performs
+            the validation.
+    """
     if len(field.domains) == 0:
         raise ValueError("Can't validate a field with no valid domains")
 
@@ -60,3 +115,31 @@ def domain_validator(field: AttributeField) -> DomainValidator:
         )
 
     return validator
+
+
+def domain_selector(field: AttributeField) -> DomainTransformer:
+    """Returns a function that builds a spark expression that calculated which
+    sub-domain of an Attribute Field each value of a Column belongs to.
+
+    Args:
+        field (AttributeField): The Attribute Field being processed against.
+
+    Returns:
+        DomainTransformer: A function that returns the spark expression that transforms
+            the column.
+    """
+
+    if len(field.domains) == 0:
+        raise ValueError("Can't select domains from a field with no valid domains")
+
+    def transformer(values: Column) -> Column:
+        domain = field.domains[0]
+        results = when(domain_contains(domain)(values), lit(domain.description))
+        for domain in field.domains[1:]:
+            results = results.when(
+                domain_contains(domain)(values), lit(domain.description)
+            )
+        results = results.otherwise(lit("__INVALID__"))
+        return results
+
+    return transformer
